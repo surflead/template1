48b9f0eda31ced5a7578d847bec0802c1ee9393c
surflead <info@surflead.pro>
2025-04-13 18:56:37 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Добро пожаловать в SurfLead!</h1>
+      <p>Это стартовый шаблон React-приложения.</p>
+    </div>
+  );
+}
+
+export default App;
